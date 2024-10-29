bab4c916d286c488b6cbc7c24d7bd9f1f1befae1
John Begood <begood.nostress.gonabeok@gmail.com>
2024-10-30 00:59:39 +0100

    voici le projet fashion

---
+++ android/app/src/main/java/com/fashionweek/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.fashionweek.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
+import { HomeComponent } from './home/home.component';
 import { Routes } from '@angular/router';
+import { ServicesComponent } from './services/services.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SignupComponent } from './signup/signup.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'',component: HomeComponent},
+  {path:'services',component: ServicesComponent},
+  {path:'projects',component: ProjectsComponent},
+  {path:'about', component: AboutUsComponent},
+  {path:'signup',component: SignupComponent}
+];
